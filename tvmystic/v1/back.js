5a71a1239c378fe67d8c072996e05abc0f991535
bobortek8 <inubyte2000@yahoo.com>
2025-07-14 13:33:38 -0400

    Add files via upload

--- tvmystic/v1/back.js
+++ tvmystic/v1/back.js
@@ -1,1 +1,25 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["POOF", "POOF"],
+        ["#", "nochan.gif"],
+        ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/5f36d726234ce10007784f2a/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-bobortek8.github.io/tvmystic/v1/ch2.gif"],
+        ["http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-bobortek8.github.io/tvmystic/v1/ch3.gif"],
+        ["http://v1.weatherscan.net/", "https://backend-bobortek8.github.io/tvmystic/v1/ch4.gif"],
+        ["https://fl3.moveonjoy.com/E_ENTERTAINMENT_TELEVISION/index.m3u8", "https://backend-bobortek8.github.io/tvmystic/v1/ch5.gif"],
+        ["https://retroblast.tv/hls/stream.m3u8", "https://backend-bobortek8.github.io/tvmystic/v1/ch6.gif"],
+        ["https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-gf2dVw-playlist.m3u8", "https://backend-bobortek8.github.io/tvmystic/v1/ch7.gif"],
+        ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-bobortek8.github.io/tvmystic/v1/ch8.gif"],
+        ["https://fl3.moveonjoy.com/MTV_CLASSIC/index.m3u8", "https://backend-bobortek8.github.io/tvmystic/v1/ch9.gif"],
+    ];
+    return channeldb;
+}
 
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
---
+++ tvmystic/v1/cable.js
@@ -0,0 +1,33 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-inubyte2000.github.io/tvmystic/v1/ch2.gif"],
+        ["index3.html#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-inubyte2000.github.io/tvmystic/v1/ch3.gif"],
+        ["index2.html#http://v1.weatherscan.net/", "https://backend-inubyte2000.github.io/tvmystic/v1/ch4.gif"],
+        ["index3.html#https://fl3.moveonjoy.com/E_ENTERTAINMENT_TELEVISION/index.m3u8", "https://backend-inubyte2000.github.io/tvmystic/v1/ch5.gif"],
+        ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-inubyte2000.github.io/tvmystic/v1/ch6.gif"],
+        ["index3.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-gf2dVw-playlist.m3u8", "https://backend-inubyte2000.github.io/tvmystic/v1/ch7.gif"],
+        ["index3.html#https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-inubyte2000.github.io/tvmystic/v1/ch8.gif"],
+        ["index3.html#https://fl3.moveonjoy.com/MTV_CLASSIC/index.m3u8", "https://backend-inubyte2000.github.io/tvmystic/v1/ch9.gif"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Bob Ross", "24/7 Joy of Painting"],
+        ["News Nation", "Non bias news 24/7"],
+        ["Weather Scan", "24/7 Weather Service"],
+        ["E!", "The most popular movies and TV shows"],
+        ["Cartoon Network", "24/7 cartoons"],
+        ["Toonami", "24/7 Cartoons and Anime"],
+        ["AWE Encore", "24/7 travel channel"],
+        ["MTV", "24/7 music channel"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
