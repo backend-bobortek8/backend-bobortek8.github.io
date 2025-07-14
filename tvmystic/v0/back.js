db8235b3e6f2324ecd765d0a7ef51b9d5be4ce87
bobortek8 <inubyte2000@yahoo.com>
2025-07-14 13:28:34 -0400

    Add files via upload

--- tvmystic/v0/back.js
+++ tvmystic/v0/back.js
@@ -1,1 +1,24 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["POOF", "POOF"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+        ["#", "nochan.gif"],
+    ];
+    return channeldb;
+}
 
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
